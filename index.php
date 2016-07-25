3dd1fb9e2f529ef65a3a6a668a00eb3f69a4586d
Gustavo Luszczynski <luszczynski@gmail.com>
2016-07-25 15:05:27 -0300

    blue

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo "<h1>Olá Mundo v1.0</h1>";
+// Create a 55x30 image
+$im = imagecreatetruecolor(200, 200);
+$white = imagecolorallocate($im, 255, 255, 255);
+$blue = imagecolorallocate($im, 0, 0, 255);
+$green = imagecolorallocate($im, 0, 255, 0);
+// Draw a white rectangle
+imagefilledrectangle($im, 0, 0, 199, 199, $blue);
+//imagefilledrectangle($im, 0, 0, 199, 199, $green);
+// Save the image
+header('Content-Type: image/png');
+imagePNG($im, './imagefilledrectangle.png');
+imagedestroy($im);
 ?>
